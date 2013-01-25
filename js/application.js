cf14d0ea5f3bbcb1ecc855adffc0a92d530d9ca5
Jake Zien <jakezien@gmail.com>
2013-01-25 02:23:16 -0500

    Revisions part 3.

--- js/application.js
+++ js/application.js
@@ -1,13 +1,13 @@
 $(window).load(function(){
       $('.flexslider').flexslider({
         animation: "slide",
-        easing: "easeInBack",
+        easing: "easeOutQuad",
         slideshow: false,
+        animationSpeed: 850,
         start: function(slider){
           $('body').removeClass('loading');
         }
-      });
-      
+      });      
 });
           
 $(document).ready(function() { 
@@ -17,22 +17,46 @@ $(document).ready(function() {
 		$('html, body').animate(animationobj, 1100);
 	});
 	
-	$("#nav-qeros").click(function () {
-		var measure=$("#qeros-cover").offset();
-		var animationobj={'scrollTop':measure.top+'px'};
-		$('html, body').animate(animationobj, 1100);
-	});
-	
 	$("#nav-willka").click(function () {
 		var measure=$("#willka-cover").offset();
 		var animationobj={'scrollTop':measure.top+'px'};
 		$('html, body').animate(animationobj, 1100);
 	});
 	
+	$("#nav-about").click(function () {
+		var measure=$("#about").offset();
+		var animationobj={'scrollTop':measure.top+'px'};
+		$('html, body').animate(animationobj, 1100);
+	});
+	
+	$("#nav-qeros").click(function () {
+		var measure=$("#qeros-cover").offset();
+		var animationobj={'scrollTop':measure.top+'px'};
+		$('html, body').animate(animationobj, 1100);
+	});
+	
 	$("#nav-donate").click(function () {
+		var measure=$("#donate").offset();
+		var animationobj={'scrollTop':measure.top+'px'};
+		$('html, body').animate(animationobj, 1100);
+	});
+	
+	$("#nav-contact").click(function () {
 		var measure=$("#footer-container").offset();
 		var animationobj={'scrollTop':measure.top+'px'};
 		$('html, body').animate(animationobj, 1100);
+	});
+	
+	$("#flexslider-students").flexslider ({
+		animation: "slide",
+        easing: "easeOutQuad",
+        slideshow: true,
+        slideshowSpeed: 5500,
+        animationSpeed: 850,
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+
 	});
 	
 });
